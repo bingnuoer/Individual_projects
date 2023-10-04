b7469909d10addc3d9ee7160ba962339dae2fa24
bingnuoer <2366858264@qq.com>
2023-10-04 16:46:52 +0800

    通过响应拦截器，统一处理接口的错误提示

--- 09-shopping/src/utils/request.js
+++ 09-shopping/src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { Toast } from 'vant'
 // 创建 axios 实例，将来对创建出来的实例，进行自定义配置
 // 好处：不会污染原始的 axios 配置
 const instance = axios.create({
@@ -20,8 +21,16 @@ instance.interceptors.request.use(function (config) {
 // 添加响应拦截器
 instance.interceptors.response.use(function (response) {
   // 2xx 范围内的状态码都会触发该函数。
-  // 对响应数据做点什么
-  return response.data // (默认axios会多包装一层data,需要响应拦截器处理一下)
+  // 对响应数据做点什么(默认axios会多包装一层data,需要响应拦截器处理一下)
+  const res = response.data
+  // console.log(res)
+  if (res.status !== 200) {
+    // 给提示-注意用全局的toast
+    Toast(res.message)
+    // 抛出一个错误的promise
+    return Promise.reject(res.message)
+  }
+  return res
 }, function (error) {
   // 超出 2xx 范围的状态码都会触发该函数。
   // 对响应错误做点什么
